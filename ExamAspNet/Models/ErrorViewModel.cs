5a8be20cf02b508349280041e0c61303f064e4f1
kdaniilm <kdaniilm@gmail.com>
2021-04-08 18:43:58 +0300

    Create Exam

---
+++ ExamAspNet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ExamAspNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
